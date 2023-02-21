61b6c6a138261f27db46965ba20facf3499bd8e9
BenomarAchraf <101630642+BenomarAchraf@users.noreply.github.com>
2023-02-22 00:30:25 +0100

    Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,44 @@
 import React from 'react'
-
-export const Navbar = () => {
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../Assets/Images/Logo.svg"
+import "@fontsource/poppins";
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+        <Link to="/">
+            <img className='logo' src={logo}/>
+        </Link>
+        <div className='pages'>
+        <Link className='pp' to="/">
+            Products
+            </Link>
+            <Link className='pp' to="/">
+           Features
+            </Link>
+            
+            <Link className='pp' to="/">
+            Pricing
+            </Link>
+            
+            <Link className='pp' to="/">
+            Support
+            </Link>
+            <div className='sign'>
+                <Link className='SignUp'>
+                    Sign Up
+                </Link>
+                <Link className='SignIn'>
+                <button className='Sign'>
+                    Sign In
+                </button>
+                </Link>
+            </div>
+            
+        </div>
+
+    </div>
   )
 }
+
+export default Navbar;
