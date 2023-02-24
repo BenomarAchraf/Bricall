2973705c69866ad603781bcaaacbd11104deec92
BenomarAchraf <101630642+BenomarAchraf@users.noreply.github.com>
2023-02-24 11:05:08 +0100

    Home

---
+++ src/Homepage/data.js
@@ -0,0 +1,56 @@
+import Card from "../Assets/Images/Card.svg"
+import Card1 from "../Assets/Images/Card1.svg"
+import Card2 from "../Assets/Images/Card2.svg"
+import Card3 from "../Assets/Images/Card3.svg"
+
+export const data = [
+    {
+        url: <img className='' src={Card}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card1}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card2}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card3}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card1}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card2}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card3}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card1}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card2}/>,
+        text: "Peinture",
+    },
+    {
+        url: <img className='' src={Card3}/>,
+        text: "Peinture",
+    },
+  ];
+  
