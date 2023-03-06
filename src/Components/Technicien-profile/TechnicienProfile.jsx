4f20ee228d0961f3682285bd7376300c4a85222e
houda el ibrahimi <92468179+houdaibr@users.noreply.github.com>
2023-03-06 23:29:21 +0100

    unutttt

---
+++ src/Components/Technicien-profile/TechnicienProfile.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./TechnicienProfile.css";
+import technicienprofile from "../../assets/technicienprofile.png";
+import category1 from "../../assets/category1.png";
+import category2 from "../../assets/category2.png";
+import category3 from "../../assets/category3.png";
+import category4 from "../../assets/category4.png";
+import category5 from "../../assets/category5.png";
+import category6 from "../../assets/category6.png";
+
+
+
+
+
+export default function TechnicienProfile(){
+    return (
+        <div className="profile-container">
+          <div className="profile-header">
+            <img className="profile-photo" src={technicienprofile} alt="Profile" />
+            <h1 className="profile-name">ALAOUI ANASS</h1>
+            <button className="profile-button">
+            <a href="/addwork
+            " > Add Work</a>
+
+              
+             </button>
+          </div>
+          <div className="profile-grid">
+            <div className="profile-grid-category">Electricité</div>
+            <div className="profile-grid-category"></div>
+            <div className="profile-grid-category"></div>
+            <div className="profile-grid-item">
+              <img src={category1} alt="Image 1" />
+              <p>Legend 1</p>
+            </div>
+            <div className="profile-grid-item">
+              <img src={category2} alt="Image 2" />
+              <p>Legend 2</p>
+            </div>
+            <div className="profile-grid-item">
+              <img src= {category3} alt="Image 3" />
+              <p>Legend 3</p>
+            </div>
+            <div className="profile-grid-item">
+              <img src={category4} alt="Image 4" />
+              <p>Legend 4</p>
+            </div>
+            <div className="profile-grid-item">
+              <img src={category5} alt="Image 5" />
+              <p>Legend 5</p>
+            </div>
+            <div className="profile-grid-item">
+              <img src={category6} alt="Image 6" />
+              <p>Legend 6</p>
+            </div>
+          </div>
+        </div>
+      );
+
+}
---
+++ src/Components/register/Register.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import './register.css';
+import registerImage from "../../assets/registerImage.png";
+
+export default function Register(){
+    
+    
+        const [fullName, setFullName] = useState("");
+        const [email, setEmail] = useState("");
+        const [username, setUsername] = useState("");
+        const [phoneNumber, setPhoneNumber] = useState("");
+        const [password, setPassword] = useState("");
+        const [confirmPassword, setConfirmPassword] = useState("");
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+        
+    };
+ return (
+    
+ <div className='register-page'>
+
+<h2 className='create-account-title'> Register Now</h2>
+<div className='container'>
+<div className='register-image'>
+<img src={registerImage} alt="avatar"/>
+</div>
+<div   >
+      
+      <form onSubmit={ handleSubmit } className="form-style-register" >
+        <div className="form-container">
+      <div className="form-row">
+          <div className="form-column">
+            <label  className="label-style" htmlFor="full-name">Full Name</label>
+            <input  className="rectangle-style" type="text" id="full-name" name="full-name" />
+          </div>
+          <div  className="form-column">
+            <label className="label-style" htmlFor="email">Email Adress</label>
+            <input className="rectangle-style" type="email" id="email" name="email" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-column">
+            <label className="label-style"  htmlFor="username">Username</label>
+            <input className="rectangle-style" type="text" id="username" name="username" />
+          </div>
+          <div className="form-column">
+            <label className="label-style" htmlFor="phone-number">Phone Number</label>
+            <input className="rectangle-style" type="tel" id="phone-number" name="phone-number" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-column">
+            <label className="label-style" htmlFor="password">Password</label>
+            <input className="rectangle-style" type="password" id="password" name="password" />
+          </div>
+          <div className="form-column">
+            <label className="label-style" htmlFor="confirm-password">Confirm Password</label>
+            <input  className="rectangle-style" type="password" id="confirm-password" name="confirm-password" />
+          </div>
+        </div>
+        </div>
+        <div >
+          <button type="submit" className="submit-button">Register</button>
+        </div>
+        <div className="already-have-account">
+        <p className="have-account-text">Already have an account?</p>
+        <a href="/login" className="register-link">Log In</a>
+      </div>
+      </form>
+     
+    </div>
+
+</div>
+ </div>
+
+ 
+
+
+ )
+
+
+}
+
+
+
+
